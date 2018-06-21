bb30fe7220401d87cbeedc6112a661772fd96f4d
Shterion Yanev <shterion.yanev@yahoo.com>
2018-06-21 16:36:54 +0200

    Modify credentials verification functnion and refactor session creating in snazzy

---
+++ Adapters/Examples/SnazzyContactsAdapter/lib/utils/snazzy.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2018 Wice GmbH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+"use strict";
+const request = require('request-promise');
+const BASE_URI = 'http://snazzycontacts.com/mp_base/json_login/login';
+
+async function createSession(config) {
+  console.log(`API KEY: ${config.apikey}`);
+
+  try {
+    const token = await getToken(config);
+    console.log(`Token: ${token}`);
+    const cookie = await getCookie(token, config);
+    console.log(`Cookie: ${config.mp_cookie}`);
+    return cookie;
+  } catch (e) {
+    console.log(`ERROR: ${e}`);
+    throw new Error(e);
+  }
+}
+
+async function getToken(config) {
+  const getTokenOptions = {
+    uri: `${BASE_URI}/get_token`,
+    headers: {
+      'X-API-KEY': config.apikey
+    }
+  };
+
+  try {
+    const tokenRequest = await request.post(getTokenOptions);
+    const tokenObject = JSON.parse(tokenRequest);
+    const { token } = tokenObject.content;
+    return token;
+  } catch (e) {
+    console.log(`ERROR: ${e}`);
+    throw new Error(e);
+  }
+}
+
+async function getCookie(token, config) {
+  try {
+    const options = {
+      uri: `${BASE_URI}/verify_credentials`,
+      json: {
+        token,
+        email: config.email,
+        password: config.password
+      },
+      headers: {
+        'X-API-KEY': config.apikey
+      }
+    };
+    const cookie = await request.post(options);
+    config.mp_cookie = cookie.content.mp_cookie;
+    return cookie.content.mp_cookie;
+  } catch (e) {
+    console.log(`ERROR: ${e}`);
+    throw new Error(e);
+  }
+}
+
+module.exports = {
+  createSession,
+  getToken,
+  getCookie
+};
--- Adapters/Examples/SnazzyContactsAdapter/verifyCredentials.js
+++ Adapters/Examples/SnazzyContactsAdapter/verifyCredentials.js
@@ -1,19 +1,47 @@
-"use strict";
+/**
+ * Copyright 2018 Wice GmbH
 
-module.exports = function verifyCredentials(credentials, cb) {
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+"use strict";
+const { getToken, getCookie, createSession } = require('./lib/utils/snazzy');
+
+async function verifyCredentials(credentials, cb) {
   console.log('Credentials passed for verification %j', credentials)
 
-  if (!credentials.email) {
-    console.log('Invalid email');
-    return cb(null, {verified: false});
+  try {
+    const cfg = {
+      apikey: credentials.apikey,
+      email: credentials.email,
+      password: credentials.password,
+      path: credentials.path
+    };
+    const token = await getToken(cfg);
+    const cookie = await getCookie(token, cfg);
+    const session = await createSession(cfg);
+
+    if (session) {
+      cb(null, { verified: true });
+      console.log('Credentials verified successfully');
+      return true;
+    }
+    throw new Error('Error in validating credentials!');
+    return false;
+  } catch (e) {
+    console.log(`${e}`);
+    throw new Error(e);
   }
-
-  if (!credentials.apikey) {
-    console.log('Invalid API key');
-    return cb(null, {verified: false});
-  }
-
-  console.log('Credentials verified successfully');
-
-  cb(null, {verified: true});
 }
+
+module.exports = verifyCredentials;

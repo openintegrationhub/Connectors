3e4e3242e74759ffaf7958d5e712696cb1d9e8e5
Shterion Yanev <shterion.yanev@yahoo.com>
2018-06-27 21:04:51 +0200

    Improve verifying credentials

--- Adapters/Examples/WiceAdapter/verifyCredentials.js
+++ Adapters/Examples/WiceAdapter/verifyCredentials.js
@@ -1,19 +1,47 @@
+/**
+ * Copyright 2018 Wice GmbH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 "use strict";
 
-module.exports = function verifyCredentials(credentials, cb) {
+const { createSession } = require('./lib/utils/wice');
+
+async function verifyCredentials(credentials, cb) {
   console.log('Credentials passed for verification %j', credentials)
+  try {
+    const cfg = {
+      apikey: credentials.apikey,
+      mandant_name: credentials.client,
+      username: credentials.username,
+      password: credentials.password,
+      apikey: credentials.apikey
+    };
 
-  if (!credentials.apikey) {
-    console.log('Invalid apikey');
-    return cb(null, {verified: false});
+    const session = await createSession(cfg);
+
+    if (session) {
+      cb(null, { verified: true });
+      console.log('Credentials verified successfully');
+      return true;
+    }
+
+    throw new Error('Error in validating credentials!');
+    return false;
+  } catch (e) {
+    console.log(`${e}`);
+    throw new Error(e);
   }
-
-  if (!credentials.mandant) {
-    console.log('Invalid mandant');
-    return cb(null, {verified: false});
-  }
-
-  console.log('Credentials verified successfully');
-
-  cb(null, {verified: true});
 }
+
+module.exports = verifyCredentials;

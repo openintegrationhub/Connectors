696fa808620bdceccee6760ce6cf71803edcf492
Unknown <shterion.yanev@yahoo.com>
2018-07-09 17:08:03 +0200

    Refactoring: modify actions

--- Adapters/Examples/WiceAdapter/lib/actions/createArticle.js
+++ Adapters/Examples/WiceAdapter/lib/actions/createArticle.js
@@ -14,12 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
  */
 
- "use strict";
+"use strict";
 const Q = require('q');
 const request = require('request-promise');
 const messages = require('elasticio-node').messages;
 
-const wice = require('./wice.js');
+const { createSession } = require('./../utils/wice');
 
 exports.process = processAction;
 
@@ -31,102 +31,98 @@ exports.process = processAction;
  */
 
 function processAction(msg, cfg) {
-
-  let reply = [];
   const self = this;
+  let reply = [];
+
+  const options = {
+    method: 'POST',
+    uri: 'https://oihwice.wice-net.de/plugin/wp_elasticio_backend/json',
+    headers: {
+      'X-API-KEY': cfg.apikey
+    }
+  };
+
   msg.body.number = 'auto';
 
-  // First create a session in Wice
-  wice.createSession(cfg, () => {
-    if (cfg.cookie) {
-
-      let article = JSON.stringify(msg.body);
-      let existingRowid = 0;
-
-      let options = {
-        method: 'POST',
-        uri: 'https://oihwice.wice-net.de/plugin/wp_elasticio_backend/json',
-        headers: {
-          'X-API-KEY': cfg.apikey
-        }
+  async function checkForExistingArticle(article, cookie) {
+    let existingRowid = 0;
+    try {
+      options.form = {
+        method: 'get_all_articles',
+        cookie,
+        search_filter: article.description
       };
 
-      function checkForExistingArticle() {
-        options.form = {
-          method: 'get_all_articles',
-          cookie: cfg.cookie,
-          search_filter: msg.body.description
-        };
-
-        return new Promise((resolve, reject) => {
-          request.post(options)
-            .then((res) => {
-              const resObj = JSON.parse(res);
-              if (resObj.loop_articles) {
-                existingRowid = resObj.loop_articles[0].rowid;
-                console.log(`Article already exists ... Rowid: ${existingRowid}`);
-              }
-              resolve(existingRowid);
-            })
-            .catch((e) => {
-              reject(e);
-            });
-        });
-      };
-
-      function createArticle() {
-        return new Promise((resolve, reject) => {
-
-          if (existingRowid > 0) {
-            msg.body.rowid = existingRowid;
-            options.form = {
-              method: 'update_article',
-              cookie: cfg.cookie,
-              data: article
-            };
-            console.log('Updating an article ...');
-
-          } else {
-            options.form = {
-              method: 'insert_article',
-              cookie: cfg.cookie,
-              data: article
-            };
-            console.log('Creating an article ...');
-          }
-          request.post(options).then((res) => {
-            const obj = JSON.parse(res);
-            reply.push(obj);
-            resolve(reply);
-          }).catch((e) => {
-            reject(e);
-          });
-        });
+      const rowid = await request.post(options);
+      const rowidObj = JSON.parse(rowid);
+      if (rowidObj.loop_articles) {
+        existingRowid = rowidObj.loop_articles[0].rowid;
+        console.log(`Article already exists ... Rowid: ${existingRowid}`);
       }
-
-      function emitData() {
-        const data = messages.newMessageWithBody({
-          "article": reply
-        });
-        self.emit('data', data);
-      }
-
-      function emitError(e) {
-        console.log('Oops! Error occurred');
-        self.emit('error', e);
-      }
-
-      function emitEnd() {
-        console.log('Finished execution');
-        self.emit('end');
-      }
-
-      Q()
-      .then(checkForExistingArticle)
-      .then(createArticle)
-      .then(emitData)
-      .fail(emitError)
-      .done(emitEnd);
+      return existingRowid;
+    } catch (e) {
+      throw new Error(e);
     }
-  });
+  }
+
+  async function createOrUpdateArticle(existingRowid, cookie) {
+    try {
+      if (existingRowid == 0) {
+        console.log('Creating article ...');
+        const input = JSON.stringify(msg.body);
+        options.form = {
+          method: 'insert_article',
+          data: input,
+          cookie
+        };
+        const article = await request.post(options);
+        return JSON.parse(article);
+      } else {
+        console.log('Updating article ...');
+        msg.body.rowid = existingRowid;
+        options.form = {
+          method: 'update_article',
+          data: JSON.stringify(msg.body),
+          cookie
+        };
+        const article = await request.post(options);
+        return JSON.parse(article);
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async function executeRequest() {
+    try {
+      const cookie = await createSession(cfg);
+      const existingRowid = await checkForExistingArticle(msg.body, cookie);
+      reply = await createOrUpdateArticle(existingRowid, cookie);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  function emitData() {
+    const data = messages.newMessageWithBody({
+      "article": reply
+    });
+    self.emit('data', data);
+  }
+
+  function emitError(e) {
+    console.log('Oops! Error occurred');
+    self.emit('error', e);
+  }
+
+  function emitEnd() {
+    console.log('Finished execution');
+    self.emit('end');
+  }
+
+  Q()
+    .then(executeRequest)
+    .then(emitData)
+    .fail(emitError)
+    .done(emitEnd);
 }

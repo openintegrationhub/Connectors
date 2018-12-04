e5173590e27b6bdee9c598427a58e1b9450370e3
Shterion <shterion.yanev@yahoo.com>
2018-12-04 14:37:34 +0100

    Fix tests

--- Transformer/Examples/WiceTransformer/spec/transform.spec.js
+++ Transformer/Examples/WiceTransformer/spec/transform.spec.js
@@ -14,25 +14,25 @@ describe('Transformation test', () => {
       .then(result => {
         expect(result.body).to.be.an('object');
         expect(result.body).to.deep.include({
-          rowid: exp.oihApplicationRecords[1].recordUid,
-          name: exp.lastName,
-          firstname: exp.firstName,
-          position: exp.jobTitle,
-          private_street: exp.addresses[0].street,
-          private_street_number: exp.addresses[0].streetNumber,
-          private_zip_code: exp.addresses[0].zipCode,
-          private_town: exp.addresses[0].city,
-          private_country: exp.addresses[0].country,
-          email: exp.contactData[3].value,
-          phone: exp.contactData[0].value,
-          mobile_phone: exp.contactData[5].value,
-          xing_url: exp.contactData[4].value,
-          last_update: exp.oihApplicationRecords[0].lastModified
+          rowid: '98765',
+          name: 'Doe',
+          firstname: 'John',
+          position: 'Sales manager',
+          private_street: 'Hohestr',
+          private_street_number: '3',
+          private_zip_code: '50667',
+          private_town: 'Cologne',
+          private_country: 'Germany',
+          email: 'jon@doe.com',
+          phone: '123456789',
+          mobile_phone: '98326307',
+          xing_url: 'xing.de/yourUsername',
+          last_update: '2018-01-02'
         });
       });
   });
 
-  it('should not produce an empty message if transformation returns undefined', () => {
+  it('should produce an empty message if transformation returns undefined', () => {
     return transformPersonFromOih.process(messages.newMessageWithBody({}))
       .then(result => {
         expect(result).to.be.undefined;
@@ -41,28 +41,27 @@ describe('Transformation test', () => {
 
   it('should handle simple person tranformation in direction to OIH', () => {
     const exp = personToOih();
-    const street = exp.private_street.split(' ');
     return transformPersonToOih.process(messages.newMessageWithBody(exp))
       .then(result => {
         expect(result.body).to.be.an('object');
         expect(result.body).to.deep.include({
-          firstName: exp.firstname,
-          lastName: exp.name,
-          jobTitle: exp.position
+          firstName: 'Mark',
+          lastName: 'Smith',
+          jobTitle: 'Marketing Manager'
         });
         expect(result.body.test).to.be.undefined;
         expect(result.body.oihApplicationRecords[0]).to.deep.include({
-          recordUid: exp.rowid
+          recordUid: 98123
         });
         expect(result.body.addresses).to.be.an('array');
         expect(result.body.addresses[0]).to.deep.include({
-          street: `${street[0]} ${street[1]}`,
-          streetNumber: parseInt(street[2], 10)
+          street: 'Main Str.',
+          streetNumber: '120'
         });
       });
   });
 
-  it('should not produce an empty message if transformation returns undefined', () => {
+  it('should produce an empty message if transformation returns undefined', () => {
     return transformPersonToOih.process(messages.newMessageWithBody({}))
       .then(result => {
         expect(result).to.be.undefined;
